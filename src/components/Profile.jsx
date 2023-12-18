bddda5f57bc564a94535349fab4670fc0748fe03
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2023-12-18 10:44:56 +0200

    Added profile data to be seen on the profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -13,9 +13,35 @@ function Profile(props) {
     <div className="profilepage">
       <h1>Profile page</h1>
       <div className="userdata">
-        {profileData.map((data) => (
-          <h3>{data.username}</h3>
-        ))}
+        <div className="yourInformation">
+          <h1>Your Information</h1>
+        </div>
+        <div className="dataContainer">
+          <div className="headers">
+            <h3>Username:</h3>
+
+            <h3>Email:</h3>
+
+            <h3>Phone:</h3>
+
+            <h3>Address:</h3>
+
+            <h3>Postcode:</h3>
+
+            <h3>City:</h3>
+          </div>
+          <div className="userInformation">
+            <h3 className="datah3">{userData.username}</h3>
+            <h3 className="datah3">{userData.email}</h3>
+            <h3 className="datah3">{userData.phone}</h3>
+            <h3 className="datah3">{userData.address}</h3>
+            <h3 className="datah3">{userData.postcode}</h3>
+            <h3 className="datah3">{userData.city}</h3>
+          </div>
+        </div>
+        <div className="updateDataButton">
+          <button>Update information</button>
+        </div>
       </div>
       <button onClick={test}>testi</button>
     </div>
