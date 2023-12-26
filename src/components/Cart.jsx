acbcc22e58605c54e46bbd5b1dd24b3f0ad44818
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2023-12-26 15:14:40 +0200

    Products are shown in cart component and they can be deleted or their amount can be decreased or increased.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,77 @@
 import React, { useEffect, useState } from "react";
 
 function Cart() {
-  const cartItems = JSON.parse(sessionStorage.getItem("Cart"));
   const [items, setItems] = useState([]);
+  const [total, setTotal] = useState();
 
   useEffect(() => {
     setItems(JSON.parse(sessionStorage.getItem("Cart")));
+    const cartItems = JSON.parse(sessionStorage.getItem("Cart"));
+    countTotal(cartItems);
   }, []);
+
+  const countTotal = (items) => {
+    var sum = 0;
+    items.forEach((item) => {
+      sum = sum + item.price * item.amount;
+    });
+    setTotal(sum);
+  };
+
+  const decrease = (id) => {
+    const updatedItems = [];
+    items.forEach((item) => {
+      if (item.id === id) {
+        item.amount = item.amount - 1;
+      }
+      if (item.amount <= 0) {
+        return;
+      } else {
+        updatedItems.push(item);
+      }
+    });
+    setItems(updatedItems);
+    sessionStorage.setItem("Cart", JSON.stringify(updatedItems));
+    countTotal(updatedItems);
+  };
+
+  const increase = (id) => {
+    const updatedItems = [];
+    items.forEach((item) => {
+      if (item.id === id) {
+        item.amount = item.amount + 1;
+      }
+      updatedItems.push(item);
+    });
+    setItems(updatedItems);
+    sessionStorage.setItem("Cart", JSON.stringify(updatedItems));
+    countTotal(updatedItems);
+  };
   return (
     <div className="CartPage">
-      {items.map((item) => (
-        <div key={item.id}>
-          <h1>
-            {item.name} amount: {item.amount}
-          </h1>
+      <div className="cartContainer">
+        <div className="cartHeader">
+          <h1>Shopping cart</h1>
         </div>
-      ))}
+        {items.map((item) => (
+          <div key={item.id} className="cartItems">
+            <h3 className="cartItemname">{item.name}</h3>
+            <h3 className="cartItemAmount">
+              {item.amount} x {item.price} €
+            </h3>
+            <button onClick={() => decrease(item.id)}>-</button>
+            <h3>{item.amount}</h3>
+            <button onClick={() => increase(item.id)}>+</button>
+          </div>
+        ))}
+
+        <div className="cartTotal">
+          <h3>Total: {total} €</h3>
+        </div>
+        <div className="chekcoutButton">
+          <button>Checkout</button>
+        </div>
+      </div>
     </div>
   );
 }
